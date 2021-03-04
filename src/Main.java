226c19902f17ab822af0abd944350df2a1a3746d
Jasmin Gelbmann <jasmin.gelbmann@gmail.com>
2021-03-04 10:02:32 +0100

    Processed

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,38 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Scanner scan = new Scanner(System.in);
         String caesar = scan.nextLine();
 
-        Random random = new Random ();
+        Random random = new Random();
         int key = random.nextInt(26);
-        char [] chars = caesar.toCharArray();
+        char[] chars = caesar.toCharArray();
 
-        for (char c : chars) {
-            c -= key;
-            System.out.print(c);
+        System.out.println(key);
+
+        for (char letter : chars) {
+
+            if (letter == 32) {
+                System.out.print(" ");
+
+            } else if ((letter + key) >= 65 && (letter + key) <= 90) {
+
+                letter += key;
+                System.out.print(letter);
+
+            } else if ((letter + key) >= 97 && (letter + key) <= 122) {
+
+                letter += key;
+                System.out.print(letter);
+
+            } else {
+
+                letter += key - 26;
+                System.out.print(letter);
+            }
+
+
         }
     }
 }
